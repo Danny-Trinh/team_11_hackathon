5e74038036cbdde242740d57220247d32b52b400
canyon_sembera@dell.com <Canyon_Sembera@Dell.com>
2021-06-22 14:49:09 -0500

    fixed minor log in problems

--- eat_website/src/pages/Login.js
+++ eat_website/src/pages/Login.js
@@ -1,4 +1,4 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { Component } from "react";
 
 export default class Login extends Component {
@@ -7,13 +7,7 @@ export default class Login extends Component {
     return (
       <div className="container">
         <h1>This is the Login page</h1>
-      </div>
-    );
-  }
-
-  login() {
-    return (
-      <form action="/html/tags/html_form_tag_action.cfm">
+        <form action="/html/tags/html_form_tag_action.cfm">
         <div className="mb-3">
           <label for="email_addr" class="form-label">Email address</label>
           <input type="email" class="form-control" id="email_addr"></input>
@@ -22,12 +16,16 @@ export default class Login extends Component {
           <label for="pwd" class="form-label">Password</label>
           <input type="password" class="form-control" id="pwd"></input>
         </div>
-        <div class="mb-3 form-check">
-          <input type="checkbox" class="form-check-input" id="remember_me"></input>
-          <label class="form-check-input" for="remember_me">Remember me</label>
-        </div>
+        
         <button type="submit" class="btn btn-primary">Submit</button>
       </form>
+      </div>
     );
   }
+
+  // login() {
+  //   return (
+      
+  //   );
+  // }
 }
