29ba5d7650be7806aa328434c0baf9944acb2d42
Le <Justina_Le@Dell.com>
2021-06-29 12:59:38 -0400

    added resource page (unfinished)

--- eat_website/src/pages/ResourcePage.js
+++ eat_website/src/pages/ResourcePage.js
@@ -4,10 +4,95 @@ export default class ResourcePage extends Component {
   render() {
     return (
       <div className="container pt-3">
-        <h1>
-          <center>Resources</center>
-        </h1>
+        <h1><center>Resources</center></h1>
+        <h2><center>Assistive Technologies</center></h2>
+
+        <h4 className="header">Visual Accessibility Features</h4>
+        <p className="separator m-0">
+          <h4 className="header mb-10">Built-In Accessibility Features</h4>
+          <h5>Windows Magnifier</h5>
+          <span class="tab"></span>Use Windows Magnifier to make all or part of your screen bigger for word and image clarity.
+          <a href='https://support.microsoft.com/en-us/windows/use-magnifier-to-make-things-on-the-screen-easier-to-see-414948ba-8b1c-d3bd-8615-0e5e32204198'>
+            <button type="button" class="btn">LINK</button></a>
+          <hr></hr>  
+          <h5>Dark Mode</h5>
+          <span class="tab"></span>Dark mode is for those who find bright displays problematic.
+          <a href='https://support.microsoft.com/en-us/windows/use-color-filters-in-windows-10-43893e44-b8b3-2e27-1a29-b0c15ef0e5ce'>
+            <button type="button" class="btn">LINK</button></a>
+          <hr></hr>
+          <h5>Immersive Reader</h5>
+          <span class="tab"></span>Use Windows Magnifier to make all or part of your screen bigger for word and image clarity.
+          <a href=''>
+            <button type="button" class="btn">LINK</button></a>
+          <hr></hr>  
+          <h5>Read Aloud</h5>
+          <span class="tab"></span>Use Windows Magnifier to make all or part of your screen bigger for word and image clarity.
+          <a href=''>
+            <button type="button" class="btn">LINK</button></a>
+          <hr></hr>  
+          <h5>Dictate</h5>
+          <span class="tab"></span>Use Windows Magnifier to make all or part of your screen bigger for word and image clarity.
+          <a href=''>
+            <button type="button" class="btn">LINK</button></a>
+          <hr></hr> 
+          <h5>NVDA (NonVisual Desktop Access)</h5>
+          <span class="tab"></span>Use Windows Magnifier to make all or part of your screen bigger for word and image clarity.
+          <a href=''>
+            <button type="button" class="btn">LINK</button></a>
+          <hr></hr> 
+          <h5>Windows Narrator</h5>
+          <span class="tab"></span>Use Windows Magnifier to make all or part of your screen bigger for word and image clarity.
+          <a href=''>
+            <button type="button" class="btn">LINK</button></a>
+
+          <hr style={{ height:10 }}></hr>  
+
+          <h4 className="header mb-10">Request Via MyIT</h4>
+          <h5>JAWS (Jobs Access with Speech)</h5>
+          <span class="tab"></span>Use Windows Magnifier to make all or part of your screen bigger for word and image clarity.
+          <a href=''>
+            <button type="button" class="btn">LINK</button></a>
+          <hr></hr>  
+          <h5>SofType5</h5>
+          <span class="tab"></span>Dark mode is for those who find bright displays problematic.
+          <a href=''>
+            <button type="button" class="btn">LINK</button></a>
+            <hr></hr>  
+        </p> 
+
+        <h4 className="header mt-4">Auditory Accessibility Features</h4>
+        <p className="separator m-0">
+          <h4 className="header" style={{ marginBottom: 10 }}>Built-In Accessibility Features</h4>
+          <h5>Windows Magnifier</h5>
+          <span class="tab"></span>Use Windows Magnifier to make all or part of your screen bigger for word and image clarity.
+          <a href=''>
+            <button type="button" class="btn">LINK</button></a>
+          <hr></hr>  
+          <h5>Dark Mode</h5>
+          <span class="tab"></span>Dark mode is for those who find bright displays problematic.
+          <a href=''>
+            <button type="button" class="btn">LINK</button></a>
+            <hr></hr>  
+          <hr style={{ height:10 }}></hr>  
+          <h4 style={{ marginBottom: 10 }}>Request Via MyIT</h4>
+          <h5>Windows Magnifier</h5>
+          <span class="tab"></span>Use Windows Magnifier to make all or part of your screen bigger for word and image clarity.
+          <a href='https://support.microsoft.com/en-us/windows/use-magnifier-to-make-things-on-the-screen-easier-to-see-414948ba-8b1c-d3bd-8615-0e5e32204198'>
+            <button type="button" class="btn">LINK</button></a>
+          <hr></hr>  
+          <h5>Dark Mode</h5>
+          <span class="tab"></span>Dark mode is for those who find bright displays problematic.
+          <a href='https://support.microsoft.com/en-us/windows/use-color-filters-in-windows-10-43893e44-b8b3-2e27-1a29-b0c15ef0e5ce'>
+            <button type="button" class="btn">LINK</button></a>
+            <hr></hr>  
+        </p> 
+        
+        <h4 className="header mt-4">Mobility Impairment</h4>
+        <p className="separator mt-0">
+          
+        </p> 
       </div>
     );
   }
 }
+
