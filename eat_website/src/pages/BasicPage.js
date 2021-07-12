ccf08c9eacc7819fbfd965000037ad966433e7b3
Le <Justina_Le@Dell.com>
2021-07-12 14:52:53 -0400

    added resource descriptions

--- eat_website/src/pages/BasicPage.js
+++ eat_website/src/pages/BasicPage.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
+import ResourcesHeader from '../img/ResourcesHeader.jpg'
 
 export default class BasicPage extends Component {
   render() {
     let example = "hello there";
     return (
       <div className="container">
-        <div style={{display:"flex", justifyContent: "center"}}>
+        <div style={{display:"flex"}}>
+        <h1> developers </h1>
+        <img src={ResourcesHeader} class="separator" style={{ height: 200}} alt=""/>
           <h1 class="separator" style={{width: 200, display:"inline-block"}}>This is a basic react component</h1>
-          <h1 class="separator" style={{width: 200, display:"inline-block", marginLeft: 10}}>This is a basic react component</h1>
+          <h1 class="separator" style={{width: 200, display:"inline-block"}}>This is a basic react component</h1>
         </div>
         
         <p>I can add expresssions by using brackets like this: {example}</p>
