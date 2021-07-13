cb956fb164b5c4cca31c1e0c9f7b1e3f7a254ae6
Le <Justina_Le@Dell.com>
2021-07-13 15:39:55 -0400

    updated about us page

--- eat_website/src/pages/AboutPage.js
+++ eat_website/src/pages/AboutPage.js
@@ -2,84 +2,58 @@ import React, { Component } from "react";
 import Brian from "../img/IMG_9277.JPG";
 import Justina from "../img/headshot.jpg";
 import Sujata from "../img/Pro pic.JPG";
-import Danny from "../img/square_headshot.jpg";
+import Danny from "../img/IMG_0931.jpg";
 import Canyon from "../img/IMG_5711.JPG";
 
+export const team = [
+    {
+      name: "Brian Akanwa",
+      intro: "Hello! My name is Brian Akanwa. I am from Houston, TX and I attend Texas Tech University!",
+      image: Brian
+    },
+    {
+      name: "Justina Le",
+      intro: "Hi, my name is Justina Le! I'm from Tampa, Florida and I go to the University of South Florida",
+      image: Justina
+    },
+    {
+      name: "Sujata Duwal",
+      intro: "Hi! My name is Sujata Duwal and I'm from San Marcos, Texas and I go to Texas State University",
+      image: Sujata
+    },
+    {
+      name: "Canyon Sembera",
+      intro: "Hello! I'm Canyon Sembera. I am from San Antonio, TX and go to Texas A&M University.",
+      image: Canyon
+    },
+    {
+      name: "Danny Trinh",
+      intro: "Last but not least! Wassup! My names Danny Trinh! I'm from Houston, TX and I attend the University of Texas.",
+      image: Danny
+    }
+  ];
+
 export default class AboutPage extends Component {
+
   render() {
     return (
       <div className="container">
-        <h1> Developers:</h1>
-        <div style={{ display: "flex", marginBottom: 0 }}>
-          <img
-            src={Brian}
-            class="separator"
-            style={{ marginRight: 0, height: 200, width: 250 }}
-            alt=""
-          />
-          <h1
-            class="separator"
-            style={{ marginRight: 45, marginLeft: 20, height: 200, width: 300 }}
-          >
-            Hello! My name is Brian Akanwa. I am from Houston, TX and I attend
-            Texas Tech University!
-          </h1>
-
-          <img
-            src={Justina}
-            class="separator"
-            style={{ marginRight: 0, height: 200, width: 300 }}
-            alt=""
-          />
-          <h1
-            class="separator"
-            style={{ marginRight: 45, marginLeft: 20, height: 200, width: 300 }}
-          >
-            Hi! My name is Justina Le and I'm from Tampa Florida and I go to
-            University of South Florida.
-          </h1>
-          <img
-            src={Sujata}
-            class="separator"
-            style={{ height: 200, width: 300 }}
-            alt=""
-          />
-          <h1
-            class="separator"
-            style={{ marginRight: 45, marginLeft: 20, height: 200, width: 300 }}
-          >
-            Hi! My name is Sujata Duwal and I'm from San Marcos, Texas and, I go
-            to Texas State University
-          </h1>
-        </div>
-        <div style={{ display: "flex", marginBottom: 0 }}>
-          <img
-            src={Canyon}
-            class="separator"
-            style={{ height: 250, width: 300 }}
-            alt=""
-          />
-          <h1
-            class="separator"
-            style={{ marginRight: 45, marginLeft: 20, height: 200, width: 300 }}
-          >
-            Hello! I'm Canyon Sembera. I am from San Antonio, TX and go to Texas
-            A&M University.
-          </h1>
-          <img
-            src={Danny}
-            class="separator"
-            style={{ height: 200, width: 300 }}
-            alt=""
-          />
-          <h1
-            class="separator"
-            style={{ marginRight: 45, marginLeft: 20, height: 200, width: 300 }}
-          >
-            Last but not least! Wassup! My names Danny Trinh! I'm from Houston,
-            TX and I attend the University of Texas.
-          </h1>
-        </div>
+        <h1 style={{margin: "10px"}}> Developers:</h1>
+        <div style={{ display: "flex", marginBottom: 0}}>
+        {team.map((data, key) => {
+        return (
+          <div class="card" style={{width: "16rem"}}>
+          <img src={data.image} class="card-img-top" alt="" />
+          <div class="card-body" key={key}>
+            <h5 class="card-title">{data.name}</h5>
+            <p class="card-text">{data.intro}</p>
+          </div>
+          </div>
+          );
+        }
+      )}
+      
+      </div>  
       </div>
     );
   }
